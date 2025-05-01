c3ffd01126402f15a63b14bd8d46789fab6d2e92
Mykola Balan <98768463+MykolaBalan22@users.noreply.github.com>
2025-05-01 15:52:04 +0300

    Change line in Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I'll be back!!! Bitches");
          System.out.println("Text from github");
-        System.out.println("Text for rebase");
+        System.out.println("Text for something");
     }
 }
